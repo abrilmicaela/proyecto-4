8b9f26036501615054204c9f71041fb77894b7ff
abrilmicaela <abrilmicaela_9@hotmail.com>
2024-06-23 19:08:13 +0200

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    console.log("DOM!")
+}
+
+)
+
+class carrito{
+    constructor({Marca,garantia,producto}){
+        this.Marca= Marca
+        this.garantia= garantia
+        this.producto= producto
+    }
+}
+
+const producto ={
+    Marca: SAMSUNG,
+    garantia: 2,
+    producto:movil,
+}
+   
+
+carrito.actualizarunidades()
+
+const total= carrito total()
+
+
+
+        
---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    console.log("DOM!")
+}
+
+)
+
+const productos= productos.map(productos=> new productos(producto))
+const productos = new productos (newproductos)})
+
